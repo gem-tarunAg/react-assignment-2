732af438a379467b10dd8dbb513679e00554e944
gem-tarunAg <tarun.agrawal@geminisolutions.com>
2023-09-05 11:07:10 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-sky-100">
+      <h1 className="text-3xl font-bold font-mono text-center py-4">React Assignment 2</h1>
+    </div>
+  );
+}
+
+export default App;
